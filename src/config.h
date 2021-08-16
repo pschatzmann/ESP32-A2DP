f204914ccf456d1a35263d29d085800d7c713566
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-08-16 09:41:29 +0200

    Automatic determination of CURRENT_ESP_IDF

--- src/config.h
+++ src/config.h
@@ -1,6 +1,11 @@
 #pragma once
 
-// Enable this if you are using a current version of ESP IDF e.g. 4.3
-#if ESP_IDF_VERSION_MAJOR > 3
+#ifdef ARDUINO
+#include "esp_arduino_version.h"
+#endif
+
+// Enable CURRENT_ESP_IDF if we are using a current version of ESP IDF e.g. 4.3
+// ESP Arduino 2.0 is using ESP IDF 4.4
+#if ESP_IDF_VERSION_MAJOR >= 4 || ESP_ARDUINO_VERSION_MAJOR >= 2
 #define CURRENT_ESP_IDF
 #endif

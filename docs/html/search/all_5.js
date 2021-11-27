8317c45df80b5c1400e56d2e61ca4a25e983a620
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-11-27 19:16:56 +0100

    doxygen

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['get2channeldata_0',['get2ChannelData',['../class_sound_data.html#aacc2d88868ceabde718113bd404452c3',1,'SoundData::get2ChannelData()'],['../class_two_channel_sound_data.html#a0a39d70aca39dbeca3d20676635b7615',1,'TwoChannelSoundData::get2ChannelData()'],['../class_one_channel_sound_data.html#a3ed4a75be0bb4dbb42752f5ae83badbb',1,'OneChannelSoundData::get2ChannelData()']]],
+  ['get2channeldata_0',['get2ChannelData',['../class_two_channel_sound_data.html#a0a39d70aca39dbeca3d20676635b7615',1,'TwoChannelSoundData::get2ChannelData()'],['../class_one_channel_sound_data.html#a3ed4a75be0bb4dbb42752f5ae83badbb',1,'OneChannelSoundData::get2ChannelData()'],['../class_one_channel8_bit_sound_data.html#a0d7a1fdf7cc8aee06c158b7dc72d491d',1,'OneChannel8BitSoundData::get2ChannelData()']]],
   ['get_5faudio_5fstate_1',['get_audio_state',['../class_bluetooth_a2_d_p_common.html#a74eadbd69b5c7adf1b190c7e41b75b10',1,'BluetoothA2DPCommon']]],
   ['get_5faudio_5ftype_2',['get_audio_type',['../class_bluetooth_a2_d_p_sink.html#a77600cb1e36b7814eb9b4126cdec62d4',1,'BluetoothA2DPSink']]],
   ['get_5fconnection_5fstate_3',['get_connection_state',['../class_bluetooth_a2_d_p_common.html#a513b32676d8fc248bb481180f832ef97',1,'BluetoothA2DPCommon']]],

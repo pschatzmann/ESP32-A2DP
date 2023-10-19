37d5f9478557d72fb7594741759588bcfe6ce612
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-10-19 07:43:43 +0200

    BluetoothA2DPSinkQueued DRAFT corrections

--- src/BluetoothA2DPSinkQueued.cpp
+++ src/BluetoothA2DPSinkQueued.cpp
@@ -2,23 +2,27 @@
 #include "BluetoothA2DPSinkQueued.h"
 #if A2DP_I2S_SUPPORT
 
-void BluetoothA2DPSinkQueued::bt_i2s_task_start_up(void)
-{
-    if ((s_ringbuf_i2s = xRingbufferCreate(i2s_ringbuffer_size, RINGBUF_TYPE_BYTEBUF)) == NULL) {
-        ESP_LOGE(BT_AV_TAG, "xRingbufferCreate");    
+void BluetoothA2DPSinkQueued::bt_i2s_task_start_up(void) {
+    ESP_LOGI(BT_APP_TAG, "ringbuffer data empty! mode changed: RINGBUFFER_MODE_PREFETCHING");
+    ringbuffer_mode = RINGBUFFER_MODE_PREFETCHING;
+    if ((s_i2s_write_semaphore = xSemaphoreCreateBinary()) == NULL) {
+        ESP_LOGE(BT_APP_TAG, "%s, Semaphore create failed", __func__);
         return;
     }
+    if ((s_ringbuf_i2s = xRingbufferCreate(RINGBUF_HIGHEST_WATER_LEVEL, RINGBUF_TYPE_BYTEBUF)) == NULL) {
+        ESP_LOGE(BT_APP_TAG, "%s, ringbuffer create failed", __func__);
+        return;
+    }
+    //xTaskCreate(bt_i2s_task_handler, "BtI2STask", 2048, NULL, configMAX_PRIORITIES - 3, &s_bt_i2s_task_handle);
     BaseType_t result = xTaskCreatePinnedToCore(ccall_i2s_task_handler, "BtI2STask", i2s_stack_size, NULL, i2s_task_priority, &s_bt_i2s_task_handle, task_core);
     if (result!=pdPASS){
         ESP_LOGE(BT_AV_TAG, "xTaskCreatePinnedToCore");
     } else {
         ESP_LOGI(BT_AV_TAG, "BtI2STask Started");
     }
-
 }
 
-void BluetoothA2DPSinkQueued::bt_i2s_task_shut_down(void)
-{
+void BluetoothA2DPSinkQueued::bt_i2s_task_shut_down(void) {
     if (s_bt_i2s_task_handle) {
         vTaskDelete(s_bt_i2s_task_handle);
         s_bt_i2s_task_handle = nullptr;
@@ -27,44 +31,80 @@ void BluetoothA2DPSinkQueued::bt_i2s_task_shut_down(void)
         vRingbufferDelete(s_ringbuf_i2s);
         s_ringbuf_i2s = nullptr;
     }
+    if (s_i2s_write_semaphore) {
+        vSemaphoreDelete(s_i2s_write_semaphore);
+        s_i2s_write_semaphore = NULL;
+    }
 
     ESP_LOGI(BT_AV_TAG, "BtI2STask shutdown");
 }
 
 /* NEW I2S Task & ring buffer */
 
-void BluetoothA2DPSinkQueued::i2s_task_handler(void *arg)
-{
+void BluetoothA2DPSinkQueued::i2s_task_handler(void *arg) {
     uint8_t *data = NULL;
     size_t item_size = 0;
+    /**
+     * The total length of DMA buffer of I2S is:
+     * `dma_frame_num * dma_desc_num * i2s_channel_num * i2s_data_bit_width / 8`.
+     * Transmit `dma_frame_num * dma_desc_num` bytes to DMA is trade-off.
+     */
+    size_t bytes_written = 0;
 
     while (true) {
-        /* receive data from ringbuffer and write it to I2S DMA transmit buffer */
-        data = (uint8_t *)xRingbufferReceive(s_ringbuf_i2s, &item_size, (portTickType)portMAX_DELAY);
+        if (pdTRUE == xSemaphoreTake(s_i2s_write_semaphore, portMAX_DELAY)) {
+            while (true) {
+                item_size = 0;
+                delay(1);
+                // receive data from ringbuffer and write it to I2S DMA transmit buffer 
+                data = (uint8_t *)xRingbufferReceiveUpTo(s_ringbuf_i2s, &item_size, (TickType_t)pdMS_TO_TICKS(i2s_ticks), i2s_write_size_upto);
+                if (item_size == 0) {
+                    ESP_LOGI(BT_APP_TAG, "ringbuffer underflowed! mode changed: RINGBUFFER_MODE_PREFETCHING");
+                    ringbuffer_mode = RINGBUFFER_MODE_PREFETCHING;
+                    break;
+                } 
 
-        if (item_size != 0){
-            i2s_write_data(data, item_size);
-            ESP_LOGD(BT_AV_TAG, "i2s_task_handler->%d",item_size);    
-            vRingbufferReturnItem(s_ringbuf_i2s, (void *)data);
-        } else {
-            ESP_LOGD(BT_AV_TAG, "i2s_task_handler-> no data");    
+                i2s_write_data(data, item_size);
+                ESP_LOGD(BT_AV_TAG, "i2s_task_handler->%d", item_size);    
+                vRingbufferReturnItem(s_ringbuf_i2s, (void *)data);
+            }
         }
     }
 }
 
 size_t BluetoothA2DPSinkQueued::write_audio(const uint8_t *data, size_t size)
 {
-    size_t result = size;
-    if (s_ringbuf_i2s==nullptr){
-        ESP_LOGE(BT_AV_TAG, "s_ringbuf_i2s is null");    
-        result = 0;
+    size_t item_size = 0;
+    BaseType_t done = pdFALSE;
+
+    if (ringbuffer_mode == RINGBUFFER_MODE_DROPPING) {
+        ESP_LOGW(BT_APP_TAG, "ringbuffer is full, drop this packet!");
+        vRingbufferGetInfo(s_ringbuf_i2s, NULL, NULL, NULL, NULL, &item_size);
+        if (item_size <= i2s_ringbuffer_prefetch_size) {
+            ESP_LOGI(BT_APP_TAG, "ringbuffer data decreased! mode changed: RINGBUFFER_MODE_PROCESSING");
+            ringbuffer_mode = RINGBUFFER_MODE_PROCESSING;
+        }
+        return 0;
     }
 
-    BaseType_t rc = xRingbufferSend(s_ringbuf_i2s, (void *)data, size, (portTickType)portMAX_DELAY);
-    if (rc==pdFALSE){
-        ESP_LOGE(BT_AV_TAG, "xRingbufferSend: %d", size);    
-        result = 0;
-    } 
-    return result;
+    done = xRingbufferSend(s_ringbuf_i2s, (void *)data, size, (TickType_t)0);
+
+    if (!done) {
+        ESP_LOGW(BT_APP_TAG, "ringbuffer overflowed, ready to decrease data! mode changed: RINGBUFFER_MODE_DROPPING");
+        ringbuffer_mode = RINGBUFFER_MODE_DROPPING;
+    }
+
+    if (ringbuffer_mode == RINGBUFFER_MODE_PREFETCHING) {
+        vRingbufferGetInfo(s_ringbuf_i2s, NULL, NULL, NULL, NULL, &item_size);
+        if (item_size >= i2s_ringbuffer_prefetch_size) {
+            ESP_LOGI(BT_APP_TAG, "ringbuffer data increased! mode changed: RINGBUFFER_MODE_PROCESSING");
+            ringbuffer_mode = RINGBUFFER_MODE_PROCESSING;
+            if (pdFALSE == xSemaphoreGive(s_i2s_write_semaphore)) {
+                ESP_LOGE(BT_APP_TAG, "semphore give failed");
+            }
+        }
+    }
+
+    return done ? size : 0;
 }
 #endif
--- src/BluetoothA2DPSinkQueued.h
+++ src/BluetoothA2DPSinkQueued.h
@@ -4,6 +4,15 @@
 
 #if A2DP_I2S_SUPPORT
 
+#define RINGBUF_HIGHEST_WATER_LEVEL    (32 * 1024)
+#define RINGBUF_PREFETCH_WATER_LEVEL   (20 * 1024)
+
+enum A2DPRingBufferMode {
+    RINGBUFFER_MODE_PROCESSING,    /* ringbuffer is buffering incoming audio data, I2S is working */
+    RINGBUFFER_MODE_PREFETCHING,   /* ringbuffer is buffering incoming audio data, I2S is waiting */
+    RINGBUFFER_MODE_DROPPING       /* ringbuffer is not buffering (dropping) incoming audio data, I2S is working */
+};
+
 /**
  * @brief The BluetoothA2DPSinkQueued is using a separate Task with an additinal Queue to write the I2S data.
  * application. 
@@ -30,13 +39,27 @@ class BluetoothA2DPSinkQueued : public BluetoothA2DPSink {
             i2s_task_priority = prio;
         }
 
+        void set_i2s_write_size_upto(size_t size){
+            i2s_write_size_upto = size;
+        }
+
+        void set_i2s_ticks(int ticks){
+            i2s_ticks = ticks;
+        }
+
     protected:
         xTaskHandle s_bt_i2s_task_handle = nullptr;  /* handle of I2S task */
         RingbufHandle_t s_ringbuf_i2s = nullptr;     /* handle of ringbuffer for I2S */
+        SemaphoreHandle_t s_i2s_write_semaphore = nullptr;
         // I2S task
         int i2s_stack_size = 2048;
-        int i2s_ringbuffer_size = 3 * 5120;
-        UBaseType_t i2s_task_priority = configMAX_PRIORITIES - 0;
+        int i2s_ringbuffer_size = RINGBUF_HIGHEST_WATER_LEVEL;
+        int i2s_ringbuffer_prefetch_size = RINGBUF_PREFETCH_WATER_LEVEL;
+        UBaseType_t i2s_task_priority = configMAX_PRIORITIES - 3;
+        A2DPRingBufferMode ringbuffer_mode = RINGBUFFER_MODE_PROCESSING;
+        size_t i2s_write_size_upto = 240 * 6;
+        int i2s_ticks = 20;
+
 
         void bt_i2s_task_start_up(void) override;
         void bt_i2s_task_shut_down(void) override;

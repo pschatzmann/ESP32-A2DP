2bc33acba084fc8ab55fa49c32d9cefa110ad961
pschatzmann <phil.schatzmann@gmail.com>
2024-04-06 20:23:18 +0200

    Doxygen remove legacy API

--- docs/html/search/classes_2.js
+++ docs/html/search/classes_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['onechannel8bitsounddata_149',['OneChannel8BitSoundData',['../class_one_channel8_bit_sound_data.html',1,'']]],
-  ['onechannelsounddata_150',['OneChannelSoundData',['../class_one_channel_sound_data.html',1,'']]]
+  ['onechannel8bitsounddata_144',['OneChannel8BitSoundData',['../class_one_channel8_bit_sound_data.html',1,'']]],
+  ['onechannelsounddata_145',['OneChannelSoundData',['../class_one_channel_sound_data.html',1,'']]]
 ];

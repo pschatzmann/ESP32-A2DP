1a3550c9d589813e36d35b2c5e3ac63f8f0d8bad
pschatzmann <phil.schatzmann@gmail.com>
2024-04-07 12:16:05 +0200

    doxygen

--- docs/html/search/classes_1.js
+++ docs/html/search/classes_1.js
@@ -1,8 +1,12 @@
 var searchData=
 [
-  ['bluetootha2dpcommon_140',['BluetoothA2DPCommon',['../class_bluetooth_a2_d_p_common.html',1,'']]],
-  ['bluetootha2dpsink_141',['BluetoothA2DPSink',['../class_bluetooth_a2_d_p_sink.html',1,'']]],
-  ['bluetootha2dpsinkqueued_142',['BluetoothA2DPSinkQueued',['../class_bluetooth_a2_d_p_sink_queued.html',1,'']]],
-  ['bluetootha2dpsource_143',['BluetoothA2DPSource',['../class_bluetooth_a2_d_p_source.html',1,'']]],
-  ['bt_5fapp_5fmsg_5ft_144',['bt_app_msg_t',['../structbt__app__msg__t.html',1,'']]]
+  ['bluetootha2dpcommon_145',['BluetoothA2DPCommon',['../class_bluetooth_a2_d_p_common.html',1,'']]],
+  ['bluetootha2dpsink_146',['BluetoothA2DPSink',['../class_bluetooth_a2_d_p_sink.html',1,'']]],
+  ['bluetootha2dpsinkqueued_147',['BluetoothA2DPSinkQueued',['../class_bluetooth_a2_d_p_sink_queued.html',1,'']]],
+  ['bluetootha2dpsource_148',['BluetoothA2DPSource',['../class_bluetooth_a2_d_p_source.html',1,'']]],
+  ['bluetoothoutput_149',['BluetoothOutput',['../class_bluetooth_output.html',1,'']]],
+  ['bluetoothoutputaudiotools_150',['BluetoothOutputAudioTools',['../class_bluetooth_output_audio_tools.html',1,'']]],
+  ['bluetoothoutputdefault_151',['BluetoothOutputDefault',['../class_bluetooth_output_default.html',1,'']]],
+  ['bluetoothoutputlegacy_152',['BluetoothOutputLegacy',['../class_bluetooth_output_legacy.html',1,'']]],
+  ['bt_5fapp_5fmsg_5ft_153',['bt_app_msg_t',['../structbt__app__msg__t.html',1,'']]]
 ];
